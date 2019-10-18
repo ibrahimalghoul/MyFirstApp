bd22dca172d866c1575a661ea045aed5fee297e7
Ibrahim Alghoul <alghoul205@gmail.com>
2019-10-18 03:03:55 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Simple Android app for GSG Bootcamp
+<img src="screenshots/Screenshot_1571356635.png" width="300"/> <img src="screenshots/Screenshot_1571356639.png" width="300"/>
---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.myfirstapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(Companion.EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+
+
+
+}
